8ad13ea8d5013eb9bd74b2bbc59643d23a576b94
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-05-09 18:43:16 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const heroSection = document.getElementById("hero");
 const skillSection = document.getElementById("techStack");
 const projectSection = document.getElementById("project");
+const c_btns = document.getElementById("certi-id");
 
 document.querySelector(".home-l").addEventListener("click", () => {
   heroSection.scrollIntoView({ behavior: "smooth" });
@@ -13,3 +14,16 @@ document.querySelector(".skills-l").addEventListener("click", () => {
 document.querySelector(".project-l").addEventListener("click", () => {
   projectSection.scrollIntoView({ behavior: "smooth" });
 });
+
+function certi_1(){
+  window.open("https://www.coursera.org/account/accomplishments/verify/9DKH8M8JSC9K");
+}
+function certi_1(){
+  window.open("https://www.coursera.org/account/accomplishments/verify/GUL48F4REY55");
+}
+function certi_1(){
+  window.open("https://www.coursera.org/account/accomplishments/verify/9DKH8M8JSC9K");
+}
+function certi_1(){
+  window.open("https://www.coursera.org/account/accomplishments/verify/9DKH8M8JSC9K");
+}
