a042bf47b2d6a1def5413c3eae02aae8846f8661
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-05-13 01:19:41 +0530

    Added Dark-Mode

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const heroSection = document.getElementById("hero");
 const skillSection = document.getElementById("techStack");
 const projectSection = document.getElementById("project");
 const c_btns = document.getElementById("certi-id");
-const light_mode = document.getElementById('light-mode');
+const light_mode = document.getElementById("light-mode");
 
 document.querySelector(".home-l").addEventListener("click", () => {
   heroSection.scrollIntoView({ behavior: "smooth" });
@@ -16,26 +16,30 @@ document.querySelector(".project-l").addEventListener("click", () => {
   projectSection.scrollIntoView({ behavior: "smooth" });
 });
 
-function certi_1(){
-  window.open("https://www.coursera.org/account/accomplishments/verify/9DKH8M8JSC9K");
+function certi_1() {
+  window.open(
+    "https://www.coursera.org/account/accomplishments/verify/9DKH8M8JSC9K"
+  );
 }
-function certi_2(){
-  window.open("https://www.coursera.org/account/accomplishments/verify/GUL48F4REY55");
+function certi_2() {
+  window.open(
+    "https://www.coursera.org/account/accomplishments/verify/GUL48F4REY55"
+  );
 }
-function certi_3(){
+function certi_3() {
   window.open("https://www.hackerrank.com/certificates/4187d9f01a74");
 }
-function certi_4(){
+function certi_4() {
   window.open("https://www.hackerrank.com/certificates/f9103af05008");
 }
-function certi_5(){
+function certi_5() {
   window.open("https://www.hackerrank.com/certificates/79cdbeb817f2");
 }
-function certi_6(){
+function certi_6() {
   window.open("https://www.hackerrank.com/certificates/bcce29463561");
 }
 
 // Dark Mode - Light Mode
-light_mode.addEventListener("click", function(){
+light_mode.addEventListener("click", function () {
   document.body.classList.toggle("light-mode");
-})
+});
